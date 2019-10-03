32445b3068c24b633ae7bb141e9aa414b586e812
Abdelkebir ELHARFALI <elharfali.abdelkebir@gmail.com>
2019-10-03 04:39:00 +0100

    add verification on admin order view

--- Block/Adminhtml/Order/View/Tab/Info/Safepay.php
+++ Block/Adminhtml/Order/View/Tab/Info/Safepay.php
@@ -1,9 +1,13 @@
 <?php
-
 namespace Godogi\Safepay\Block\Adminhtml\Order\View\Tab\Info;
+
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class Safepay extends \Magento\Sales\Block\Adminhtml\Order\AbstractOrder
 {
+    protected $_storeManager;
+    protected $_scopeConfig;
     protected $_safepayEntryFactory;
     /**
      * Constructor
@@ -19,9 +23,13 @@ class Safepay extends \Magento\Sales\Block\Adminhtml\Order\AbstractOrder
         \Magento\Framework\Registry $registry,
         \Magento\Sales\Helper\Admin $adminHelper,
         \Godogi\Safepay\Model\EntryFactory $safepayEntryFactory,
+        StoreManagerInterface $storeManager,
+        ScopeConfigInterface $scopeConfig,
         array $data = []
     ) {
         $this->_safepayEntryFactory = $safepayEntryFactory;
+        $this->_storeManager = $storeManager;
+        $this->_scopeConfig = $scopeConfig;
         parent::__construct($context, $registry, $adminHelper, $data);
     }
 
@@ -40,5 +48,41 @@ class Safepay extends \Magento\Sales\Block\Adminhtml\Order\AbstractOrder
         $method = $payment->getMethodInstance();
         $methodTitle = $method->getTitle();
         return $payment->getMethod();
+    }
+    public function validateCallback($tracker) {
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+        $safepayTest = $this->_scopeConfig->getValue(\Godogi\Safepay\Model\SafepayConfigProvider::SAFEPAY_TEST_CONFIG, $storeScope);
+        $safepaySandbox = $this->_scopeConfig->getValue(\Godogi\Safepay\Model\SafepayConfigProvider::SAFEPAY_SANDBOX_CONFIG, $storeScope);
+        $safepayProduction = $this->_scopeConfig->getValue(\Godogi\Safepay\Model\SafepayConfigProvider::SAFEPAY_PRODUCTION_CONFIG, $storeScope);
+
+  			if($safepayTest) {
+  				$url = "https://sandbox.api.getsafepay.com/order/v1/".$tracker;
+  			} else {
+  				$url = "https://api.getsafepay.com/order/v1/".$tracker;
+  			}
+  			$ch =  curl_init($url);
+		    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+		    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+		    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)');
+  			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  			$result = curl_exec($ch);
+  			if (curl_errno($ch)) {
+  			   return false;
+  			}
+  			curl_close($ch);
+  			$result_array = json_decode($result);
+  			if(empty($result_array->status->errors)) {
+  				$state = $result_array->data->state;
+  				if($state === "TRACKER_ENDED") {
+  					return true;
+  				} else {
+  					return false;
+  				}
+  			} else {
+  				return false;
+  			}
     }
 }
